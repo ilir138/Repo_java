e69e0c43bcda7e14b51c3970249cc0581e307cde
ilir138 <ilir.mh@gmail.com>
2020-04-29 21:31:28 -0400

    First Homework

---
+++ src/Homeworks/HW1.java
@@ -0,0 +1,57 @@
+package Homeworks;
+
+public class HW1 {
+    public static void main(String[] args) {
+
+        byte numberOfHoursInDay = 24;
+
+        short maxDaysInYear = 365;
+
+        int totalEmployeesInOrganization = 2566;
+
+        long populationInChina = 1435004385;
+
+        float interestRate = 0.03f;
+
+        double balanceInBankAccount = 12300;
+
+        boolean sunRiseFromWest = false;
+
+        char firstNameInitial = 'I';
+        char lastNameInitial = 'M';
+
+        String myName = "Ilir Mullahasani";
+
+
+
+        System.out.println("Numbers of hours in a Day -> " + numberOfHoursInDay);
+        System.out.println("Number of max no. of days in a years -> " + maxDaysInYear);
+        System.out.println("Total employees in an Organization -> " + totalEmployeesInOrganization);
+        System.out.println("Population in China -> " + populationInChina);
+        System.out.println("Interest rate right now is -> " + interestRate);
+        System.out.println("Balance in a bank account -> " + balanceInBankAccount);
+        System.out.println("Does the sun rise from the west? " + sunRiseFromWest);
+        System.out.println("My name Initials -> " +firstNameInitial + "." +lastNameInitial+".");
+        System.out.println("My name is: " +myName);
+
+
+
+        
+        double cTemp = 25;
+        double fTemp = 9.0/5 * (cTemp) + 32;
+        double kTemp = 270;
+        double fTemp2 = 9.0/5 * (kTemp - 273) + 32;
+        double kTemp2 = cTemp + 273;
+        double cTemp2 = kTemp - 273;
+        double kTemp3 = 5.0/9 * (fTemp - 32) + 273;
+
+
+        System.out.println("Temperature in Fahrenheit = " + fTemp);
+        System.out.println("Temperature in Fahrenheit = " + fTemp2);
+        System.out.println("Temperature in Kelvin = " + kTemp2);
+        System.out.println("Temperature in Celsius = " + cTemp2);
+        System.out.println("Temperature in Kelvin = " + kTemp3);
+
+
+    }
+}
--- src/String_HW/StringQuestions.java
+++ src/String_HW/StringQuestions.java
@@ -4,6 +4,6 @@ public class StringQuestions {
     public static void main(String[] args) {
         String name = "peace required";
         System.out.println("Message is: " + name);
-        
+
     }
 }
